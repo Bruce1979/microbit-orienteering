d31561d39246f3ecf2da37bb78ab48cb57f2ba4f
Bruce Fuda <bruce@fuda.me>
2018-07-03 14:17:58 +1000

    Updates to the codebase to allow for multiple courses attached to multiple flags. Includes descriptive comments for teachers to understand the code. Untested on device at this stage.

--- compass.py
+++ compass.py
@@ -1,79 +1,183 @@
+""" compass.py
+
+Author: Bruce Fuda
+Developed for the microbit-orienteering project at Telopea Park School
+to assist with teaching Digital Technologies content in Year 8 integrated PE
+License: CC-BY-4.0
+
+This module is uploaded to the "compasses" that will be used by students
+completing the orienteering course.  Compasses operate in three modes:
+    - 'compass' mode is the default and displays the current directional
+      reading as detected by the micro:bit's magnetometer.
+    - 'checking' mode is used to activate the radio and scan for nearby signals
+      from flags.  This is a separate mode to the compass to help minimise
+      power usage.
+    - 'showing' mode is used to display the flags that the compass has visited
+      on the given course.  After displaying them all, it automatically reverts
+      to 'compass' mode.
+
+Comments prefaced with ## are descriptive to explain the code's purpose to the
+teacher/student.  They would normally be written in a more succinct way, but
+are kept long form to assist with clarity for non-programmers.
+
+"""
+
+## import everything from the microbit module for Python
 from microbit import *
+## import radio module so that calling radio functions is easier
 import radio
 
-images = {
-  '0' : Image.HAPPY,
-  '1' : Image.SAD,
-  '2' : Image.GIRAFFE,
-  '3' : Image.RABBIT,
-  '4' : Image.DUCK,
-  '5' : Image.GHOST,
-  '6' : Image.DIAMOND,
-  '7' : Image.SWORD,
-  '8' : Image.PACMAN,
-  '9' : Image.HOUSE
+## create a dictionary that maps the IDs for each of the flag images
+## to the images. This is used in communication and must be present
+## in both the compass and flag to ease sending/receiving messages.
+## REMEMBER THAT IF THIS LIST IS ALTERED, CHANGES MUST BE REFLECTED IN THE
+## CORRESPONDING IMAGES LIST IN THE flag.py FILE.
+images = {  # image IDs must be letter strings
+  'A' : Image.HAPPY,
+  'B' : Image.SAD,
+  'C' : Image.GIRAFFE,
+  'D' : Image.RABBIT,
+  'E' : Image.DUCK,
+  'F' : Image.GHOST,
+  'G' : Image.DIAMOND,
+  'H' : Image.SWORD,
+  'I' : Image.PACMAN,
+  'J' : Image.HOUSE
 }
 
-MY_ID = 'BLUE'
+## Set the ID for the compass and the course it is currently completing
+MY_ID = 'BLUE'   # must be unique across all compasses participating
+MY_COURSE = '1'  # must be a single digit
 
-COMPASS = [Image.CLOCK12,
-           Image.CLOCK1,
-           Image("00009:00090:00900:00000:00000"),
-           Image.CLOCK2,
-           Image.CLOCK3,
-           Image.CLOCK4,
-           Image("00000:00000:00900:00090:00009"),
-           Image.CLOCK5,
-           Image.CLOCK6,
-           Image.CLOCK7,
-           Image("00000:00000:00900:09000:09000"),
-           Image.CLOCK8,
-           Image.CLOCK9,
-           Image.CLOCK10,
-           Image("90000:09000:00900:00000:00000"),
-           Image.CLOCK11,
-           ]
+## Creates a dictionary of images that reflect the position of the needle
+## to be shown on the micro:bit display.  The key is used to determine the
+## required angle to Magnetic North from the current position as determined by
+## the bearing reading of the built-in magnetometer.
+## NOTE: since decimal numbers can be problematic in computer representation,
+## we have rounded values down to the nearest integer (and will use this
+## convention for all of our calculations)
+COMPASS = {0 : Image("00900:00900:00900:00000:00000"),
+           22 : Image("00090:00090:00900:00000:00000"),
+           45 : Image("00009:00090:00900:00000:00000"),
+           67 : Image("00000:00099:00900:00000:00000"),
+           90 : Image("00000:00000:00999:00000:00000"),
+           112 : Image("00000:00000:00900:00099:00000"),
+           135 : Image("00000:00000:00900:00090:00009"),
+           157 : Image("00000:00000:00900:00090:00090"),
+           180 : Image("00000:00000:00900:00900:00900"),
+           202 : Image("00000:00000:00900:09000:09000"),
+           225 : Image("00000:00000:00900:09000:90000"),
+           247 : Image("00000:00000:00900:99000:00000"),
+           270 : Image("00000:00000:99900:00000:00000"),
+           292 : Image("00000:99000:00900:00000:00000"),
+           315 : Image("90000:09000:00900:00000:00000"),
+           337 : Image("09000:09000:00900:00000:00000")
+           }
 
+## create a list that will track the checkpoints in the order they are visited
 checkpoints = []
-mode = "compass"
 
+## configure the radio to transmit at its weakest strength, saving power
+## and reducing the range of the broadcast signal so that compasses must
+## get close for it to be detected
 radio.config(power=0)
 radio.on()
+
+## set the starting mode for the compass - it will begin in 'compass' mode
+mode = "compass"
+
+## force compass calibration each time the micro:bit is restarted to maximise
+## accuracy of the magnetometer
 compass.calibrate()
 
+## This loop ensures that the code runs as long as the flag has power
 while True:
-    
+
   # mode switching
+  ## switchign modes operates differently on the compass since none of the
+  ## modes require any direct user input / manipulation.  Thus, we start
+  ## by checking for button presses and, if they are detected, we trigger
+  ## the relevant mode switch
+  ## pressing button B at any point will switch the micro:bit into 'showing'
+  ## mode.  We clear the display of anything as we switch.
   if button_b.is_pressed():
     mode = "showing"
     display.clear()
+  ## button A is used to switch between compass and checking modes, so we
+  ## check which mode we are currently in and switch to the other if it is
+  ## pressed.  Again, clear the display of the current info as we switch.
   elif button_a.is_pressed():
     if mode == "compass":
       mode = "checking"
     elif mode == "checking":
       mode = "compass"
     display.clear()
-  
+
   # define modes
+  ## in compass mode, we get a heading from the compass and then calculate the
+  ## relevant bearing to north that we will be showing on our display.  There
+  ## is a bit of maths going on here, so we'll do it step by step.
   if mode == "compass":
-    # Try to keep the needle pointed in (roughly) the correct direction
+    ## We get the current heading as calculated from the built-in magnetometer/
+    ## compass.  This will be an integer between 0 and 360. Since North is
+    ## at zero and we are using regions of 22.5 degress between each measurable
+    ## reading, we subtract our heading from 11 (half of 22.5, rounded down) to
+    # get a bearing to North.
+    north_bearing = 11 - compass.heading()
+    ## We now need to convert this into one of the values in our COMPASS
+    ## dictionary.  To do this, we first divide by 22.5, which is our angle
+    ## between each of our displayable compass points. This tells us how many
+    ## multiples of 22.5 we are from North, and we round this down to the
+    ## nearest integer by converting using the int() function
+    points_from_north = int(north_bearing / 22.5)
+    ## Now, we multiply this value by 22.5, and once again round it down to
+    ## match one of our values in the COMPASS dictionary
+    pos = int(points_from_north * 22.5)
+    ## Point the needle to the nearest "cardinal point" bearing towards
+    ## Magnetic North by displaying the correct image from our dictionary
+    display.show(COMPASS[pos])
+    ## Pause momentarily before getting another reading
     sleep(100)
-    pos = int(((15 - compass.heading()) // 22.5)) % 16
-    display.show(COMPASS[pos])       
-    #pos = -int(compass.heading()/22.5)
-    #display.show(COMPASS[pos])
+  ## once we enter checking mode, we'll begin looking for a radio signal from
+  ## a nearby checkpoint
   elif mode == "checking":
+    ## attempt to catch any messages being sent from nearby checkpoints
     checkpoint = radio.receive()
     if checkpoint:
-      display.show(images[checkpoint])
-      if checkpoint not in checkpoints:
-        checkpoints.append(checkpoint)
-        radio.send(MY_ID)
+      ## before we do anything, we need to check to see if this checkpoint is
+      ## on our course.  We begin by extracting the first character from the
+      ## message we received and comparing that to our course
+      checkpoint_course = checkpoint[0]
+      if checkpoint_course == MY_COURSE:
+        ## if this checkpoint is on our course, then we need to get the
+        ## checkpoints id from its message, which is every everything after the
+        ## first character (i.e. everything in the message from index 1 onwards)
+        checkpoint_id = checkpoint[1:]
+        ## we then show the relevant image by looking it up in our images
+        ## dictionary
+        display.show(images[checkpoint_id])
+        ## Next, we see if we have recorded this checkpoint before and, if we
+        ## haven't already stored its id in our list of checkpoints, we add it
+        if checkpoint_id not in checkpoints:
+          checkpoints.append(checkpoint_id)
+          ## Once we've added the checkpoint into our list of visited
+          ## checkpoints, send a message to the checkpoint to indicate which
+          ## course we are doing and what our id is so it can store this
+          ## information.  We send a message containing our id, prepended with
+          # the course we are currently doing.
+          radio.send(MY_COURSE+MY_ID)
+    ## pause for a moment so we don't spam the radio signals in the area
+    ##  too much
     sleep(200)
+  ## in showing mode, we iterate over the checkpoints we've saved and display
+  ## them one at a time, for one second each.  This preserves the order they
+  ## were visited / saved
   elif mode == "showing":
     for cp in checkpoints:
       display.show(images[cp])
       sleep(1000)
+    ## after showing them all on the display, clear the display and switch back
+    ## to compass mode, after a short delay.
     display.clear()
     sleep(1000)
     mode = "compass"
--- flag.py
+++ flag.py
@@ -1,49 +1,156 @@
+""" flag.py
+
+Author: Bruce Fuda
+Developed for the microbit-orienteering project at Telopea Park School
+to assist with teaching Digital Technologies content in Year 8 integrated PE
+License: CC-BY-4.0
+
+This module is uploaded to the "flags" that will be the destination points
+for the orienteering course.  Flags operate in two modes:
+    - 'flag' mode is the default and displays the flag's identifier image and
+      transmits that flag's presence to nearby compasses.  It performs a
+      cyclical broadcast of its identifier, prepended with each of the courses
+      on which it is present.  This is used by receiving compasses to determine
+      if the flag is on their course and, if it is, to identify the flag as a
+      recipient for the compass id.  Flags store compass ids and the course that
+      compass is on so that they can be displayed when in 'vistors' mode.
+    - 'visitors' mode is used to display the compasses that have visited the
+      flag for any given course.  After displaying the list of visitors for a
+      chosen course, it automatically reverts to 'flag' mode.
+
+Comments prefaced with ## are descriptive to explain the code's purpose to the
+teacher/student.  They would normally be written in a more succinct way, but
+are kept long form to assist with clarity for non-programmers.
+
+"""
+
+## import everything from the microbit module for Python
 from microbit import *
+## import radio module so that calling radio functions is easier
 import radio
 
-images = {
-  '0' : Image.HAPPY,
-  '1' : Image.SAD,
-  '2' : Image.GIRAFFE,
-  '3' : Image.RABBIT,
-  '4' : Image.DUCK,
-  '5' : Image.GHOST,
-  '6' : Image.DIAMOND,
-  '7' : Image.SWORD,
-  '8' : Image.PACMAN,
-  '9' : Image.HOUSE
+## create a dictionary that maps the IDs for each of the flag images
+## to the images. This is used in communication and must be present
+## in both the compass and flag to ease sending/receiving messages.
+## REMEMBER THAT IF THIS LIST IS ALTERED, CHANGES MUST BE REFLECTED IN THE
+## CORRESPONDING IMAGES LIST IN THE compass.py FILE.
+images = {  # image IDs must be letter strings
+  'A' : Image.HAPPY,
+  'B' : Image.SAD,
+  'C' : Image.GIRAFFE,
+  'D' : Image.RABBIT,
+  'E' : Image.DUCK,
+  'F' : Image.GHOST,
+  'G' : Image.DIAMOND,
+  'H' : Image.SWORD,
+  'I' : Image.PACMAN,
+  'J' : Image.HOUSE
 }
 
-MY_ID = '0'
+## Set the ID for the flag and the courses this flag is part of
+MY_ID = 'A'   # must be key in list of images and unique across all flags
+MY_COURSES = ['1','2','5']  # must be a list of single digits
+
+## create a list that will track the visitors in the order they arrive
 visitors = []
+
+## configure the radio to transmit at its weakest strength, saving power
+## and reducing the range of the broadcast signal so that compasses must
+## get close for it to be detected
 radio.config(power=0)
 radio.on()
-mode="flag"
 
+## set the starting mode for the flag - it will begin in 'flag' mode
+mode = "flag"
+
+## This loop ensures that the code runs as long as the flag has power
 while True:
-    
-  # mode switching
-  if button_a.is_pressed():
-    if mode == "flag":
-      mode = "visitors"
-    elif mode == "visitors":
-      mode = "flag"
-    display.clear()
-  
+
   # define modes
-  if mode == "flag":  
-    display.show(images[MY_ID])
-    radio.send(MY_ID)
-    visitor = radio.receive()
-    if visitor:
-      if visitor not in visitors:
-        visitors.append(visitor)
-    sleep(1000)
+  ## flag mode broadcasts the flag ID and tracks visitors
+  if mode == "flag":
+    ## if button A is pressed/held down while in this mode, the flag switches
+    ## into visitors mode and the display is cleared.
+    if button_a.is_pressed():
+      mode = "visitors"
+      display.clear()
+    ## otherwise, we begin transmission/receiving of data
+    else:
+      ## when in flag mode, we display the flag's identity so it is visible
+      display.show(images[MY_ID])
+      ## we need to transmit not just the flag's ID, but the courses it is
+      ## a part of.  The message sent consists of the flag's ID prepended with
+      ## the course it is a part of.  Where it is on multiple courses, we need
+      ## to rotate through the list of courses, and transmit each combination
+      ## separately.
+      for course in MY_COURSES:
+        ## construct the message to be sent, using the current course and the
+        ## flag ID.  Send it using the radio protocol.
+        radio.send(course+MY_ID)
+        ## attempt to catch any receiving message from a visitor if they are
+        ## on the correct course.
+        visitor = radio.receive()
+        if visitor:
+          ## if the visitor sent a response, check to see which course it is on
+          ## by extracting the first character of its message
+          visitor_course = visitor[0]
+          ## if the visitor is on one of the courses the flag is on...
+          if visitor_course in MY_COURSES:
+            ## and if the visitor hasn't already visited this flag on this
+            ## course
+            if visitor not in visitors:
+              ## add the visitor to the list of visitors.  Note we store the
+              ## whole message including the course number, since a visitor
+              ## may visit the flag multiple times for different courses and
+              ## we need to be able to distinguish between them.
+              visitors.append(visitor)
+        ## pause code execution for a short time between each transmission
+        sleep(600)
+  ## visitors mode displays the visitors from the selectec course
   elif mode == "visitors":
-    for v in visitors:
-      display.scroll(v)
-      sleep(1000)
-    display.clear()
-    sleep(1000)
-    mode = "flag"
-    
+    ## we need to identify which course to display.  By default, we'll extract
+    ## the course id that appears first in the list of courses.  This always
+    ## has an index of 0.
+    course_to_view = 0 # index of course to display
+    ## while in this mode, we'll continuously wait for a button press.  We can
+    ## do this with an infinite loop, which we'll break from when we return
+    ## to flag mode
+    while True:
+      ## button B will be used to cycle through the list of courses this flag
+      ## is a part of - it does this by advancing the index by 1 through the
+      ## list, and rotating back to 0 if we end up "off the end" by advancing
+      ## to the length of the list
+      if button_b.is_pressed():
+        course_to_view += 1
+        ## if the index stored in course_to_view ever gets to the length of the
+        ## lst, it has gone too far and should be set to 0 (the start) instead.
+        if course_to_view >= len(MY_COURSES):
+          course_to_view = 0
+        ## Show which course is currently selected on the micro:bit display
+        display.show(MY_COURSES[0])
+        sleep(600)
+      ## button A will be used to show the visitors for the selected course
+      if button_a.is_pressed():
+        ## We will iterate over the visitors list, which will preserve the
+        ## order they visited the flag.
+        for v in visitors:
+          ## extract the course id from the data that is stored so that we only
+          ## show visitors for the chosen course
+          visitor_course = v[0]
+          ## compare the visitors course with the chosen course and, if they
+          ## match, we'll display the visitor's id
+          if visitor_course == course_to_view:
+            ## the data stored includes the course as the first character in the
+            ## data string, so the visitor's id is everything from the second
+            ## character (the character at index 1) through to the end.  Extract
+            ## this and then scroll in on the micro:bit display.
+            visitor_id = v[1:]
+            display.scroll(visitor_id)
+            ## pause for one second after each so that we can distinguish
+            ## between them all
+            sleep(1000)
+        ## once we've displayed all of the relevant visitors, clear the display
+        ## switch to flag mode and exit this loop to revert to the main loop
+        display.clear()
+        mode = "flag"
+        break
